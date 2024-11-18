45cd281fb762a03cedf0a4f079af7336f6538287
Mohamed Khalil <mahamedkhalil5527@gmail.com>
2024-11-18 03:58:18 +0200

    add(add note cubit to add note bottom sheet)

---
+++ lib/views/widgets/add_note_form.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:my_notes/views/widgets/custom_button.dart';
+import 'package:my_notes/views/widgets/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subtTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 35,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hint: 'title',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              subtTitle = value;
+            },
+            hint: 'content',
+            maxLines: 7,
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
